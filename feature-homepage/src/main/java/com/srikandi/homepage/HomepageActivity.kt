f7496d051b6e46c46646be07bedb8ccdbbc9a2eb
Otniel <otnieldocs@gmail.com>
2021-02-10 22:21:47 +0700

    fix: remove not needed gradle config, add showcase fragment

--- feature-homepage/src/main/java/com/srikandi/homepage/HomepageActivity.kt
+++ feature-homepage/src/main/java/com/srikandi/homepage/HomepageActivity.kt
@@ -2,10 +2,16 @@ package com.srikandi.homepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.srikandi.common.extensions.addFragment
+import com.srikandi.homepage.screen.HomepageShowcaseFragment
 
 class HomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        if (savedInstanceState == null) {
+            addFragment<HomepageShowcaseFragment>(R.id.homepage_activity_framelayout)
+        }
     }
 }
---
+++ feature-homepage/src/main/java/com/srikandi/homepage/screen/HomepageShowcaseFragment.kt
@@ -0,0 +1,11 @@
+package com.srikandi.homepage.screen
+
+import com.srikandi.common.mvrx.MvRxFragment
+import com.srikandi.homepage.R
+
+class HomepageShowcaseFragment : MvRxFragment(R.layout.homepage_fragment_showcase) {
+    override fun invalidate() {
+        TODO("Not yet implemented")
+    }
+
+}
