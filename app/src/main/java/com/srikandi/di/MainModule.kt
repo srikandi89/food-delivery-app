d3d6fc8a0e5e8d51111fbb790b622b05ca71dab6
Otniel <otnieldocs@gmail.com>
2021-02-13 16:39:04 +0700

    feat: setup retrofit instance

--- app/src/main/java/com/srikandi/di/MainModule.kt
+++ app/src/main/java/com/srikandi/di/MainModule.kt
@@ -1,8 +1,35 @@
 package com.srikandi.di
 
+import com.srikandi.BuildConfig
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class MainModule {
-    // todo : provide something required in general here
+    @Singleton
+    @Provides
+    fun getRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+    @Singleton
+    @Provides
+    fun getOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun getHttpLoggingInterceptor() = HttpLoggingInterceptor().also {
+        it.level = HttpLoggingInterceptor.Level.BODY
+    }
 }
